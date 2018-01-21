d478067a014cc73dd93d8021c3c3dabcae40b89c
Ryan Greenberg <ryangreenberg@gmail.com>
2018-01-21 14:48:47 -0800

    Support command `node`

--- lib/vrsn.rb
+++ lib/vrsn.rb
@@ -28,6 +28,7 @@ module Vrsn
   COMMANDS = [
     cmd('git', DDVERSION, match_stdout(/^git version ([.0-9]+)\b/)),
     cmd('java', SDVERSION, match_stderr(/java version "(.+?)"/)),
+    cmd('node', DDVERSION, match_stdout(/^v(.+?)$/)),
     cmd('npm', DDVERSION, match_stdout(/^(.+?)$/)),
     cmd('python', DDVERSION, match_stderr(/^Python (.+?)$/)),
     cmd('ruby', DDVERSION, match_stdout(/^ruby (.+?)p/)),
